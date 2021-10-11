650d0b026594548e81d074cf09e55aceb67b2a91
witnip <hi@witnip.com>
2021-10-11 16:32:46 +0530

    recycler adapter done

---
+++ app/src/main/java/com/witnip/newsapp/WebViewActivity.java
@@ -0,0 +1,13 @@
+package com.witnip.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_webview);
+    }
+}
--- app/src/main/java/com/witnip/newsapp/adapters/Adapter.java
+++ app/src/main/java/com/witnip/newsapp/adapters/Adapter.java
@@ -1,28 +1,60 @@
 package com.witnip.newsapp.adapters;
 
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.witnip.newsapp.R;
+import com.witnip.newsapp.WebViewActivity;
 import com.witnip.newsapp.databinding.NewsItemBinding;
+import com.witnip.newsapp.models.ArticlesItem;
+
+import java.util.ArrayList;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    Context mContext;
+    ArrayList<ArticlesItem> articles;
+
+    public Adapter(Context mContext, ArrayList<ArticlesItem> articles) {
+        this.mContext = mContext;
+        this.articles = articles;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(mContext).inflate(R.layout.news_item,parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
 
+        ArticlesItem article = articles.get(position);
+
+        holder.binding.cardView.setOnClickListener(view -> {
+            Intent intent = new Intent(mContext, WebViewActivity.class);
+            intent.putExtra("url",article.getUrl());
+            mContext.startActivity(intent);
+        });
+
+        holder.binding.mainHeading.setText(article.getTitle());
+        holder.binding.contentHeading.setText(article.getDescription());
+        Glide.with(mContext).load(article.getUrlToImage()).into(holder.binding.mainImage);
+        holder.binding.author.setText(String.format("By %s", article.getAuthor()));
+        holder.binding.time.setText(String.format("Publised At: %s", article.getPublishedAt()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return articles.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
