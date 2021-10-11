e6f3f9fda5a08373c1372506fb1cf9846e91005f
witnip <hi@witnip.com>
2021-10-11 14:38:53 +0530

    Design part done

---
+++ app/src/main/java/com/witnip/newsapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.witnip.newsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.witnip.newsapp.adapter.PagerAdapter;
+import com.witnip.newsapp.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    PagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        adapter = new PagerAdapter(getSupportFragmentManager(),6);
+        binding.container.setAdapter(adapter);
+
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                binding.container.setCurrentItem(tab.getPosition());
+                if(tab.getPosition() == 0 ||
+                tab.getPosition() == 1 ||
+                tab.getPosition() == 2 ||
+                tab.getPosition() == 3 ||
+                tab.getPosition() == 4 ||
+                tab.getPosition() == 5){
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        binding.container.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(binding.tabLayout));
+    }
+}
---
+++ app/src/main/java/com/witnip/newsapp/fragment/ScienceFragment.java
@@ -0,0 +1,25 @@
+package com.witnip.newsapp.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.witnip.newsapp.R;
+
+public class ScienceFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_science, container, false);
+    }
+}
